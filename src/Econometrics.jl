7fe9771ec7747da947e1902ac7e36515047bfeb3
Christian Groll <groll.christian.edu@gmail.com>
2014-01-28 10:50:35 +0100

    copula file added to module

--- src/Econometrics.jl
+++ src/Econometrics.jl
@@ -5,8 +5,17 @@ using TimeData
 using Winston
 using NLopt
 
+export                                  # important functions
+ishighest,
+islowest,
+ranks,
+plot
+
 include("autocorr.jl")
+include("copula.jl")
 include("garch.jl")
 include("returns.jl")
+
+
 
 end # module
